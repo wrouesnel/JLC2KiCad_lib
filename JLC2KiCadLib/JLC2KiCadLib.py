8056b1b17e2ce3d3d108d528f663bf1ddb6ce6d1
Will Rouesnel <wrouesnel@wrouesnel.com>
2025-08-07 19:50:30 +1000

    Extract the main parser to be a global
    This makes it easy to use the utility as a component
    of other scripts.

--- JLC2KiCadLib/JLC2KiCadLib.py
+++ JLC2KiCadLib/JLC2KiCadLib.py
@@ -8,6 +8,121 @@ from . import helper
 from .footprint.footprint import create_footprint, get_footprint_info
 from .symbol.symbol import create_symbol
 
+parser = argparse.ArgumentParser(
+    description="take a JLCPCB part # and create the according component's kicad's library",
+    epilog="example use : \n	JLC2KiCadLib C1337258 C24112 -dir My_lib -symbol_lib My_Symbol_lib --no_footprint",
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+)
+
+parser.add_argument(
+    "components",
+    metavar="JLCPCB_part_#",
+    type=str,
+    nargs="+",
+    help="List of JLCPCB part # from the components you want to create",
+)
+
+parser.add_argument(
+    "-dir",
+    dest="output_dir",
+    type=str,
+    default="JLC2KiCad_lib",
+    help="Base directory for output library files",
+)
+
+parser.add_argument(
+    "--no_footprint",
+    dest="footprint_creation",
+    action="store_false",
+    help="Use --no_footprint if you do not want to create the footprint",
+)
+
+parser.add_argument(
+    "--no_symbol",
+    dest="symbol_creation",
+    action="store_false",
+    help="Use --no_symbol if you do not want to create the symbol",
+)
+
+parser.add_argument(
+    "-symbol_lib",
+    dest="symbol_lib",
+    type=str,
+    default=None,
+    help='Set symbol library name, default is "default_lib"',
+)
+
+parser.add_argument(
+    "-symbol_lib_dir",
+    dest="symbol_lib_dir",
+    type=str,
+    default="symbol",
+    help='Set symbol library path, default is "symbol" (relative to OUTPUT_DIR)',
+)
+
+parser.add_argument(
+    "-footprint_lib",
+    dest="footprint_lib",
+    type=str,
+    default="footprint",
+    help='Set footprint library name,  default is "footprint"',
+)
+
+parser.add_argument(
+    "-models",
+    dest="models",
+    nargs="*",
+    choices=["STEP", "WRL"],
+    type=str,
+    default="STEP",
+    help="Select the 3D model you want to use. Default is STEP. If both are selected, only the STEP model will be added to the footprint (the WRL model will still be generated alongside the STEP model). If you do not want any model to be generated, use the --models without arguments",
+)
+
+parser.add_argument(
+    "-model_dir",
+    dest="model_dir",
+    type=str,
+    default="packages3d",
+    help='Set directory for storing 3d models, default is "packages3d" (relative to FOOTPRINT_LIB)',
+)
+
+parser.add_argument(  # argument to skip already existing files and symbols
+    "--skip_existing",
+    dest="skip_existing",
+    action="store_true",
+    help="Use --skip_existing if you want do not want to replace already existing footprints and symbols",
+)
+
+parser.add_argument(
+    "-model_base_variable",
+    dest="model_base_variable",
+    type=str,
+    default="",
+    help="Use -model_base_variable if you want to specify the base path of the 3D model using a path variable. If the specified variable starts with '$' it is used 'as-is', otherwise it is encapsulated: $(MODEL_BASE_VARIABLE)",
+)
+
+parser.add_argument(
+    "-logging_level",
+    dest="logging_level",
+    type=str,
+    default="INFO",
+    choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+    help="Set logging level. If DEBUG is used, the debug logs are only written in the log file if the option  --log_file is set ",
+)
+
+parser.add_argument(
+    "--log_file",
+    dest="log_file",
+    action="store_true",
+    help="Use --log_file if you want logs to be written in a file",
+)
+
+parser.add_argument(
+    "--version",
+    action="version",
+    version=f"%(prog)s {__version__}",
+    help="Print versin number and exit",
+)
 
 def add_component(component_id, args):
     logging.info(f"creating library for component {component_id}")
@@ -55,124 +170,7 @@ def add_component(component_id, args):
             skip_existing=args.skip_existing,
         )
 
-
 def main():
-    parser = argparse.ArgumentParser(
-        description="take a JLCPCB part # and create the according component's kicad's library",
-        epilog="example use : \n	JLC2KiCadLib C1337258 C24112 -dir My_lib -symbol_lib My_Symbol_lib --no_footprint",
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-    )
-
-    parser.add_argument(
-        "components",
-        metavar="JLCPCB_part_#",
-        type=str,
-        nargs="+",
-        help="List of JLCPCB part # from the components you want to create",
-    )
-
-    parser.add_argument(
-        "-dir",
-        dest="output_dir",
-        type=str,
-        default="JLC2KiCad_lib",
-        help="Base directory for output library files",
-    )
-
-    parser.add_argument(
-        "--no_footprint",
-        dest="footprint_creation",
-        action="store_false",
-        help="Use --no_footprint if you do not want to create the footprint",
-    )
-
-    parser.add_argument(
-        "--no_symbol",
-        dest="symbol_creation",
-        action="store_false",
-        help="Use --no_symbol if you do not want to create the symbol",
-    )
-
-    parser.add_argument(
-        "-symbol_lib",
-        dest="symbol_lib",
-        type=str,
-        default=None,
-        help='Set symbol library name, default is "default_lib"',
-    )
-
-    parser.add_argument(
-        "-symbol_lib_dir",
-        dest="symbol_lib_dir",
-        type=str,
-        default="symbol",
-        help='Set symbol library path, default is "symbol" (relative to OUTPUT_DIR)',
-    )
-
-    parser.add_argument(
-        "-footprint_lib",
-        dest="footprint_lib",
-        type=str,
-        default="footprint",
-        help='Set footprint library name,  default is "footprint"',
-    )
-
-    parser.add_argument(
-        "-models",
-        dest="models",
-        nargs="*",
-        choices=["STEP", "WRL"],
-        type=str,
-        default="STEP",
-        help="Select the 3D model you want to use. Default is STEP. If both are selected, only the STEP model will be added to the footprint (the WRL model will still be generated alongside the STEP model). If you do not want any model to be generated, use the --models without arguments",
-    )
-
-    parser.add_argument(
-        "-model_dir",
-        dest="model_dir",
-        type=str,
-        default="packages3d",
-        help='Set directory for storing 3d models, default is "packages3d" (relative to FOOTPRINT_LIB)',
-    )
-
-    parser.add_argument(  # argument to skip already existing files and symbols
-        "--skip_existing",
-        dest="skip_existing",
-        action="store_true",
-        help="Use --skip_existing if you want do not want to replace already existing footprints and symbols",
-    )
-
-    parser.add_argument(
-        "-model_base_variable",
-        dest="model_base_variable",
-        type=str,
-        default="",
-        help="Use -model_base_variable if you want to specify the base path of the 3D model using a path variable. If the specified variable starts with '$' it is used 'as-is', otherwise it is encapsulated: $(MODEL_BASE_VARIABLE)",
-    )
-
-    parser.add_argument(
-        "-logging_level",
-        dest="logging_level",
-        type=str,
-        default="INFO",
-        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
-        help="Set logging level. If DEBUG is used, the debug logs are only written in the log file if the option  --log_file is set ",
-    )
-
-    parser.add_argument(
-        "--log_file",
-        dest="log_file",
-        action="store_true",
-        help="Use --log_file if you want logs to be written in a file",
-    )
-
-    parser.add_argument(
-        "--version",
-        action="version",
-        version=f"%(prog)s {__version__}",
-        help="Print versin number and exit",
-    )
-
     args = parser.parse_args()
 
     helper.set_logging(args.logging_level, args.log_file)
